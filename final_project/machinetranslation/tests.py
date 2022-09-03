a26bb936f2d3fa24b5fb5e26be9c2dbd34e88297
Aakash Bhumbla <aakash.bhumbla@gmail.com>
2022-09-03 08:46:45 +0000

    TST: adds unit tests

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,2 +1,16 @@
+import unittest
 from translator import french_to_english, english_to_french
 
+class TestFrenchToEnglish(unittest.TestCase): 
+    def testf2e(self): 
+        self.assertEqual(french_to_english('Bonjour'), 'Hello') 
+    def testnotf2e(self):
+        self.assertNotEqual(french_to_english('Rien'), 'Something')
+
+class TestEnglishToFrench(unittest.TestCase): 
+    def teste2f(self): 
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+    def testnote2f(self):
+        self.assertNotEqual(english_to_french('Something'), 'Rien')
+
+unittest.main()
